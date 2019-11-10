990646d1dd6bdd4b55ecce36514247a63b4f505e
BorisKamenetskiy <boris.kamenetskiy@onetick.com>
2019-11-10 12:47:23 +0000

    Homework terraform-2 without * completed

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "app_external_ip" {
-   value = "${join(",",google_compute_instance.app.*.network_interface.0.access_config.0.nat_ip)}"
-}
-
-output "loadbalancer_ip" {
-   value = "${google_compute_global_forwarding_rule.default.ip_address}"
-}
-
